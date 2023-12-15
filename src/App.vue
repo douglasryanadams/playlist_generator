0375fe760bc6c27107d0626daa61dd94bb5e9f15
Douglas Adams <douglasryanadams@gmail.com>
2023-12-14 18:22:01 -0800

    Last 4 hours recent played

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { useSpotifyTokenStore } from "./stores/spotifyTokenStore";
-import { useRoute, useRouter } from "vue-router";
-import { inject } from "vue";
+import {useSpotifyTokenStore} from "./stores/spotifyTokenStore";
+import {useRoute, useRouter} from "vue-router";
+import {inject} from "vue";
+
 const router = useRouter();
 const route = useRoute();
 
@@ -14,36 +15,65 @@ const utcToLocalTime = (timestamp) => {
   return utcDate.toString();
 };
 
+const fourHoursAgo = () => {
+  return new Date().getTime() - (4 * 60 * 60 * 1000)  // hours * minutes * seconds * milliseconds
+}
+
+const fourHoursAgoReadable = () => {
+  const four_hours_ago = new Date(fourHoursAgo());
+  return four_hours_ago.getHours() + ":" + String(four_hours_ago.getMinutes()).padStart(2, '0')
+}
+
 const downloadRecentlyPlayed = async (event) => {
+  console.log(new Date(fourHoursAgo()).toString())
   const params = {
-    limit: 50,
+    limit: 5,
+    after: fourHoursAgo(),
   };
   const paramString = new URLSearchParams(Object.entries(params)).toString();
-  const response = await fetch(
-    `https://api.spotify.com/v1/me/player/recently-played?${paramString}`,
-    { headers: { Authorization: `Bearer ${spotifyTokenStore.token}` } }
-  );
-  handleExpiredToken(response, spotifyTokenStore);
-  const rJson = await response.json();
 
+  let next_url = `https://api.spotify.com/v1/me/player/recently-played?${paramString}`
   const recentlyPlayed = [];
-  for (const item of rJson.items) {
-    recentlyPlayed.push({
-      trackName: item.track.name,
-      trackArtist: item.track.artists.map(({ name }) => name).join(", "),
-      trackId: item.track.id,
-      trackUrl: item.track.external_urls.spotify,
-      playedAt: item.played_at,
-      playedAtLocal: utcToLocalTime(item.played_at),
-    });
+  console.log(next_url)
+
+  while (next_url != null) {
+    const response = await fetch(
+        next_url,
+        {headers: {Authorization: `Bearer ${spotifyTokenStore.token}`}}
+    );
+    handleExpiredToken(response, spotifyTokenStore);
+    const rJson = await response.json();
+    next_url = rJson.next
+    console.log(next_url)
+    console.log(rJson)
+
+    for (const item of rJson.items) {
+      recentlyPlayed.push({
+        trackId: item.track.id,
+        trackName: item.track.name,
+        trackArtist: item.track.artists.map(({name}) => name).join(", "),
+        trackDurationMilliseconds: item.track.duration_ms,
+        trackUrl: item.track.external_urls.spotify,
+        trackUri: item.track.uri,
+        contextType: item.context.type,
+        contextUrl: item.context.external_urls.spotify,
+        contextUri: item.context.uri,
+        playedAt: item.played_at,
+        playedAtLocal: utcToLocalTime(item.played_at),
+      });
+    }
   }
 
-  let csv = "data:text/csv;charset=utf-8,";
-  csv += "id,name,artist,url,played_at_utc,played_at_local\r\n";
+  let csv = []
+  csv.push("data:text/csv;charset=utf-8,");
+  csv.push("id,name,artist,duration_milliseconds,url,uri,context,context_url,context_uri,played_at_utc,played_at_local\r\n");
   for (const play of recentlyPlayed) {
-    csv += `"${play.trackId}","${play.trackName}","${play.trackArtist}","${play.trackUrl}","${play.playedAt}","${play.playedAtLocal}"\r\n`;
+    csv.push(`"${play.trackId}","${play.trackName}","${play.trackArtist}",`)
+    csv.push(`"${play.trackDurationMilliseconds}", "${play.trackUrl}", "${play.trackUri}", `)
+    csv.push(`"${play.contextType}","${play.contextUrl}","${play.contextUri}",`)
+    csv.push(`"${play.playedAt}", "${play.playedAtLocal}"\r\n`)
   }
-  const encodedUri = encodeURI(csv);
+  const encodedUri = encodeURI(csv.join(""));
   const link = document.createElement("a");
   link.setAttribute("href", encodedUri);
   link.setAttribute("download", "recently_played.csv");
@@ -55,8 +85,10 @@ const downloadRecentlyPlayed = async (event) => {
 
 const signout = (event) => {
   spotifyTokenStore.clear();
-  router.push({ name: "sign-in" });
+  router.push({name: "sign-in"});
 };
+
+
 </script>
 
 <template>
@@ -64,32 +96,32 @@ const signout = (event) => {
     <div class="site-title">Playlist Generator</div>
     <div class="flex ml-auto">
       <Button
-        label="Back"
-        icon="pi pi-arrow-left"
-        severity="info"
-        v-if="route.name === 'playlist-generation'"
-        @click="() => router.push({ name: 'song-selection' })"
+          label="Back"
+          icon="pi pi-arrow-left"
+          severity="info"
+          v-if="route.name === 'playlist-generation'"
+          @click="() => router.push({ name: 'song-selection' })"
       />
       <Button
-        label="Recently Played"
-        icon="pi pi-download"
-        severity="help"
-        @click="downloadRecentlyPlayed"
-        v-if="spotifyTokenStore.signedIn"
-        class="ml-3"
+          :label="'Recently Played (Since About ' +  fourHoursAgoReadable() + ')'"
+          icon="pi pi-download"
+          severity="help"
+          @click="downloadRecentlyPlayed"
+          v-if="spotifyTokenStore.signedIn"
+          class="ml-3"
       />
       <Button
-        label="Sign Out"
-        icon="pi pi-times"
-        severity="danger"
-        @click="signout"
-        v-if="spotifyTokenStore.signedIn"
-        class="ml-3"
+          label="Sign Out"
+          icon="pi pi-times"
+          severity="danger"
+          @click="signout"
+          v-if="spotifyTokenStore.signedIn"
+          class="ml-3"
       />
     </div>
   </div>
   <div class="px-5">
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
